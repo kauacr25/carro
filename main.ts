3e28c40ef99df24630f71cd538546de87b690923
kauacr25 <132594510+kauacr25@users.noreply.github.com>
2023-05-04 12:38:39 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Yes)
+})
